e0e7d63dc08d5dc829bb6999be3780c4607576ac
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2017-03-04 18:44:32 +0000

    form types for the entities

---
+++ src/Geekdoos/DoctorsBundle/Form/ConsultationMedsType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Geekdoos\DoctorsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ConsultationMedsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ->add('meds', 'entity', array('label'=>'Medicament', 'class' => 'GeekdoosDoctorsBundle:Meds','property' => 'name',))
+            ->add('meds', 'entity', array(
+                'label'=>'Medicament', 
+                'class' => 'GeekdoosDoctorsBundle:Meds',
+                'query_builder' => function(\Doctrine\ORM\EntityRepository $er) {
+                    return $er->createQueryBuilder('m')
+                        ->where('m.count > 0')
+                        ->andWhere('m.expdate > CURRENT_DATE()')
+                        ->orderBy('m.name', 'ASC');
+                    },
+                ))
+            ->add('count','text', array('label'=>'Nombre d\'unité'))
+            ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Geekdoos\DoctorsBundle\Entity\ConsultationMeds'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Geekdoos_consultationmeds';
+    }
+}
---
+++ src/Geekdoos/DoctorsBundle/Form/imageType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Geekdoos\DoctorsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class imageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder 
+            ->add('file')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Geekdoos\DoctorsBundle\Entity\image'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'Geekdoos_imagetype';
+    }
+}
